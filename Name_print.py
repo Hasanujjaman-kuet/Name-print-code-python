012ae00cc12c33ee4ae2b5b7b7d98c79d249a7fa
Muhammad Hasanujjaman <108048293+Hasanujjaman-kuet@users.noreply.github.com>
2022-10-02 08:42:34 +0600

    Update Name_print.py

--- Name_print.py
+++ Name_print.py
@@ -1,4 +1,14 @@
-first_name = input ("Enter first name: ")
-last_name = input ("Enter last name: ")
-full_name = first_name + ' ' + last_name
-print("full_name: " + full_name)
+# Below Line will Take Inputs
+"""
+fvsdgvsd
+dfgdfg
+sgdfg
+"""
+first_name = input("Enter First Name: ")
+last_name = input("Enter Last Name: ")
+
+# will Merge into Full name
+full_name = first_name +" "+ last_name
+
+# will print output
+print("Full Name: "+full_name)
